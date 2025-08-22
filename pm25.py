28480962b673577af5f8379bce94f23ff96d07f3
tommy <tommy@gmail.com>
2025-08-22 21:35:28 +0800

    完成pm2.5數據取得

--- pm25.py
+++ pm25.py
@@ -1,7 +1,7 @@
 import pymysql
 import requests
 
-table_str="""
+table_str = """
 create table if not exists pm25(
 id int auto_increment primary key,
 site varchar(25),
@@ -13,55 +13,54 @@ unique key site_time (site,datacreationdate)
 )
 """
 
-url="https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=540e2ca4-41e1-4186-8497-fdd67024ac44&limit=1000&sort=datacreationdate%20desc&format=JSON"
+url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=540e2ca4-41e1-4186-8497-fdd67024ac44&limit=1000&sort=datacreationdate%20desc&format=JSON"
 
-sqlstr="insert ignore into pm25(site,county,pm25,datacreationdate,itemunit)\
+sqlstr = "insert ignore into pm25(site,county,pm25,datacreationdate,itemunit)\
       values(%s,%s,%s,%s,%s)"
 
-conn,cursor=None,None
+conn, cursor = None, None
+
 
 def open_db():
-    global conn,cursor
+    global conn, cursor
     try:
-        conn=pymysql.connect(
-        host="localhost",
-        user="root",
-        password="",
-        port=3306,
-        database="demo"
+        conn = pymysql.connect(
+            host="localhost", user="root", password="", port=3306, database="demo"
         )
 
-        #print(conn)
-        cursor=conn.cursor()
+        # print(conn)
+        cursor = conn.cursor()
         print("資料庫開啟成功")
     except Exception as e:
         print(e)
+
 
 def close_db():
     if conn is not None:
         conn.close()
         print("資料庫關閉成功")
 
+
 def get_open_data():
-    resp=requests.get(url,verify=False)
-    datas=resp.json()['records']
-    values=[list(data.values()) for data in datas if list(data.values())[2]!=""]
+    resp = requests.get(url, verify=False)
+    datas = resp.json()["records"]
+    values = [list(data.values()) for data in datas if list(data.values())[2] != ""]
     return values
+
 
 def write_to_sql():
     try:
-        values=get_open_data()
-        if len(values)==0:
+        values = get_open_data()
+        if len(values) == 0:
             print("目前無資料")
             return
-        size=cursor.executemany(sqlstr,values)
+        size = cursor.executemany(sqlstr, values)
         conn.commit()
         print(f"寫入{size}資料成功")
     except Exception as e:
         print(e)
 
 
-
-open_db()
-write_to_sql()
-close_db()
+# open_db()
+# write_to_sql()
+# close_db()
